5400412fd29b40ddffdf1087fd1640a6a4888dd6
sucharitha28589 <suchi.yudy@gmail.com>
2019-04-22 11:00:10 +0530

    add a java programme

---
+++ test/suchi.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class suchi {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j <=i; j++) {
+				System.out.print(j);
+				
+			}System.out.println(" ");
+			
+		}
+	}
+
+}
